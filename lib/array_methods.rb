61d19eb2e37be9507a2e1a20fa1bc634ed9bb40f
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-02 18:07:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  return array.first
 end
 
 def using_last(array)
-
+  return array.last
 end
 
 def using_size(array)
-
+  return array.size
 end
